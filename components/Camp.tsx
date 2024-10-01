4ced0f6acdcd331e308c0d3641ccaf591e98b2db
Nick Frazao <nicolas_frazao@hotmail.com>
2024-10-01 15:27:31 -0300

    Update Camp.tsx

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -8,6 +8,8 @@ interface CampProps {
   peopleJoined: string;
 }
 
+/*eslint-disable*/
+
 const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps) => {
   return (
     <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
@@ -71,7 +73,7 @@ const Camp = () => {
             <strong>Feeling Lost</strong> And Not Knowing The Way?
           </h2>
           <p className="regular-14 xl:regular-16 mt-5 text-white">
-            Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. That's why we are here for those of you who want to start an adventure
+            Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. That&apos;s why we are here for those of you who want to start an adventure
           </p>
           <Image 
             src="/quote.svg"
